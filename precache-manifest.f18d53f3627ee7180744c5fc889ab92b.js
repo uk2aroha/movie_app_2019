2b54dc38ac527babc16a44b053c5338cb0b73268
Kimikhwan <uk2aroha@naver.com>
2020-06-12 15:31:54 +0900

    Updates

---
+++ precache-manifest.f18d53f3627ee7180744c5fc889ab92b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97545e0dd232adfe4e67f6698fb700c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69f1a1cfe7a4eeb8d5bb",
+    "url": "/static/css/main.37981529.chunk.css"
+  },
+  {
+    "revision": "7676ec8dfa3a757764b3",
+    "url": "/static/js/2.cba50508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cba50508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f1a1cfe7a4eeb8d5bb",
+    "url": "/static/js/main.23ddf052.chunk.js"
+  },
+  {
+    "revision": "bf29cfb97a2a9e6276e0",
+    "url": "/static/js/runtime-main.e0617c52.js"
+  }
+]);

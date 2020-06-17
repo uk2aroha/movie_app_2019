aad610de4dc4796c92fcc66be30bab25b1e24439
Kimikhwan <uk2aroha@naver.com>
2020-06-17 16:15:12 +0900

    Routing

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css"
 
-function Movie({id, year, title, summary, poster, genres}) {
-    return ( 
+function Movie({ id, year, title, summary, poster, genres }) {
+    return (
+        
         <div className ="movie">
+            <Link to={{
+                pathname: `/movie/${id}`,
+                state:{
+                    year,
+                    title,
+                    summary,
+                    poster,
+                    genres
+                }
+                }}
+            >
             <img src={poster} alt={title} title={title} />
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
@@ -15,8 +28,9 @@ function Movie({id, year, title, summary, poster, genres}) {
                     ))}
                 </ul>
                 <p className="movie__summary">{summary.slice(0,180)}...</p>
-            </div>
-        </div>
+            </div>   
+            </Link>
+        </div> 
     );
 }
 
